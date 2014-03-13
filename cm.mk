81200035798458a761f612e22b52caa68a3d9109
Peter Ezetta <protoCall7@gmail.com>
2014-03-12 19:23:12 -0700

    Initial Commit

--- cm.mk
+++ cm.mk
@@ -13,6 +13,6 @@ $(call inherit-product, device/unknown/fiber_ondam975q5/device_fiber_ondam975q5.
 ## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := fiber_ondam975q5
 PRODUCT_NAME := cm_fiber_ondam975q5
-PRODUCT_BRAND := unknown
+PRODUCT_BRAND := onda
 PRODUCT_MODEL := fiber_ondam975q5
 PRODUCT_MANUFACTURER := unknown
---
+++ vendorsetup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: vendorsetup.sh
+# 
+#         USAGE: ./vendorsetup.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Peter H. Ezetta (protoCall7), peter.ezetta@zonarsystems.com
+#  ORGANIZATION: R&D
+#       CREATED: 03/11/2014 20:31
+#      REVISION:  ---
+#===============================================================================
+
+add_lunch_combo cm_fiber_ondam975q5-eng
